fe3a9ed3745415109900865d207190888ffe0d03
João <joaop.oliveira@ua.pt>
2022-01-05 20:03:18 +0000

    Added Menu and Menu Input Validation

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+void showMenu();
+void validateUserOptionInput(int* pOption);
 
 typedef struct InvoiceEntries{
     char product[50];
@@ -11,8 +15,12 @@ typedef struct InvoiceEntries{
 int main()
 {
     InvoiceEntry invoice;
+    int option;
 
-    strcpy(invoice.product, "apple");
+    showMenu();
+    validateUserOptionInput(&option);
+
+    strcpy(invoice.product, "apples");
     invoice.amount = 2;
     invoice.unitPrince = 1.4;
 
@@ -24,5 +32,26 @@ int main()
 
 void showMenu()
 {
+    printf("*************************\n");
+    printf("    Menu\n");
+    printf("-------------------------\n");
+    printf(" 1. Add Invoice\n");
+    printf(" 2. Search Invoice\n");
+    printf(" 3. Show All Invoices\n");
+    printf(" 4. Exit\n");
+    printf("-------------------------\n");
+    printf(" Pick your option...\n");
+    printf(".........................\n");
+}
 
+void validateUserOptionInput(int* pOption)
+{
+    bool invalidOption = true;
+    do{
+        scanf("%d", pOption);
+        invalidOption = (*pOption <1 || *pOption > 4);
+
+        if(invalidOption) printf("Invalid Menu Option! Pick your Option...\n");
+    }
+    while(invalidOption);
 }
