ef4da6e49b06cf525d7b5dcac3eb669b769d26fc
João <joaop.oliveira@ua.pt>
2022-01-05 21:06:36 +0000

    Added Function to Create Invoices

--- main.c
+++ main.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
 void showMenu();
 void validateUserOptionInput(int* pOption);
-
+struct InvoiceEntries *getNewInvoice();
 
 struct Items{
     char product[50];
@@ -24,11 +23,15 @@ typedef struct InvoiceEntries InvoiceEntry;
 
 int main()
 {
-    InvoiceEntry invoice;
+    InvoiceEntry *first = NULL;
     int option;
 
+    ///Program Loop
     showMenu();
     validateUserOptionInput(&option);
+
+    first = getNewInvoice();
+    printf("First entry at %p, number of orders %d, next Invoice at %p\n", first, first->orderSize, first->next);
 
     switch(option)
     {
@@ -76,4 +79,17 @@ void validateUserOptionInput(int* pOption)
         if(invalidOption) printf("Invalid Menu Option! Pick your Option...\n");
     }
     while(invalidOption);
+}
+
+InvoiceEntry *getNewInvoice()
+{
+    InvoiceEntry *newInvoice = NULL;
+    newInvoice = malloc(sizeof(InvoiceEntry));
+    if(newInvoice == NULL) printf("Error allocating memory!\n");
+    else
+    {
+        newInvoice->next = NULL;
+        newInvoice->orderSize = 2;
+    }
+    return newInvoice;
 }
